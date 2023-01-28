9f8c6cbfd089b0bf93ddeb00fa67ae503c267e26
Castellano Santiago <santiagocastellano01@gmail.com>
2023-01-28 16:43:00 -0300

    vercel config

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import uvicorn
-
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -27,7 +25,3 @@ async def get_video(url: str = Form(), only_audio: bool = Form()):
         filename=file_path,
         media_type='text/mp4'
     )
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
