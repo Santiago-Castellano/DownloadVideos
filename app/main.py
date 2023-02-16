27af4e3f41b664898b2c542a0e8322bf073dae38
Castellano Santiago <santiagocastellano01@gmail.com>
2023-02-16 00:13:18 -0300

    refactor architecture

--- app/main.py
+++ app/main.py
@@ -39,6 +39,3 @@ async def get_video(url: str = Form(), only_audio: bool = Form()):
         filename=out_file,
         media_type='text/mp4'
     )
-
-if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=8080)
